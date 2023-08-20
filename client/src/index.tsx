bafa080878123dd9a0ce36e542d0507768d0c118
Sergei Shologin <s.shologin@yandex.ru>
2023-08-20 22:41:42 +0600

    Add basic routing

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Main } from "./pages/main";
+import { Root } from "./components/Root";
+import { Register } from "./pages/register";
+import { Auth } from "./pages/auth";
+import { Room } from "./pages/room"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+        children: [
+            {
+              index: true,
+              element: <Main/>
+            },
+            {
+                path: "/register",
+                element: <Register/>
+            },
+            {
+                path: "/login",
+                element: <Auth/>
+            },
+            {
+                path: "/profile",
+                element: <Main/>
+            },
+            {
+                path: "/room",
+                element: <Room/>
+            }
+        ]
+    },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
